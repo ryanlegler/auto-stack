933749c7f0309ed7e1646fd4e7113a7910e53790
Ryan Legler <ryan.legler@gmail.com>
2023-12-10 22:01:29 -0800

    chore: cleans up unused stories stuff and old styledsystem folder

--- styled-system/jsx/auto-stack.mjs
+++
@@ -1,9 +0,0 @@
-import { createElement, forwardRef } from 'react'
-import { styled } from './factory.mjs';
-import { getAutoStackStyle } from '../patterns/auto-stack.mjs';
-
-export const AutoStack = /* @__PURE__ */ forwardRef(function AutoStack(props, ref) {
-  const { breakpoint, direction, vAlign, hAlign, ...restProps } = props
-const styleProps = getAutoStackStyle({breakpoint, direction, vAlign, hAlign})
-return createElement(styled.div, { ref, ...styleProps, ...restProps })
-})
